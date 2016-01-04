b8b794de609700342531942bc0e7ac7bddcfa475
danielpilla <daniel.b.pilla@gmail.com>
2016-01-04 10:30:31 -0500

    Update dpi-simple-table.js

--- dpi-simple-table.js
+++ dpi-simple-table.js
@@ -110,46 +110,52 @@ define(["jquery", "text!./dpi-simple-table.css"], function($, cssContent) {'use 
 						html += "'";
 					}
 				  //if just links is selected, check for links and convert
-				if(layout.linkColumns && !layout.imageColumns){
-					if(cell.qText.slice(0,4).toLowerCase()==='http'){
-						html += '> <a href="' + cell.qText + '" target="_blank">' + cell.qText + '</a></td>';
+				if(cell.qText !== undefined) {
+					if(layout.linkColumns && !layout.imageColumns){
+						if(cell.qText.slice(0,4).toLowerCase()==='http'){
+							html += '> <a href="' + cell.qText + '" target="_blank">' + cell.qText + '</a></td>';
+						}
+						else if(cell.qText.slice(0,3).toLowerCase()==='www'){
+							html += '> <a href="http://' + cell.qText + '" target="_blank">' + cell.qText + '</a></td>';
+						}
+						else{
+							html += '>' + cell.qText + '</td>';
+						}
 					}
-					else if(cell.qText.slice(0,3).toLowerCase()==='www'){
-						html += '> <a href="http://' + cell.qText + '" target="_blank">' + cell.qText + '</a></td>';
+					 
+					  //if just images is selected, check for images and convert
+					else if(layout.imageColumns && !layout.linkColumns){
+						if(~cell.qText.toLowerCase().indexOf('img.') || ~cell.qText.toLowerCase().indexOf('.jpg') || ~cell.qText.toLowerCase().indexOf('.gif') || ~cell.qText.toLowerCase().indexOf('.png')){
+						    html += '> <img src="' + cell.qText + '" height=' + layout.imageHeight + '></td>';
+						}
+						else{
+							html += '>' + cell.qText + '</td>';
+						}
 					}
-					else{
-						html += '>' + cell.qText + '</td>';
+					  //if both images and links are selected, if an image, convert and add a link
+					  //if not an image, just convert to link
+					else if(layout.imageColumns && layout.linkColumns){
+						if(~cell.qText.toLowerCase().indexOf('img.') || ~cell.qText.toLowerCase().indexOf('.jpg') || ~cell.qText.toLowerCase().indexOf('.gif') || ~cell.qText.toLowerCase().indexOf('.png')){
+						    html += '> <a href="' + cell.qText + '" target="_blank"><img src="' + cell.qText + '"" height=' + layout.imageHeight + '></a></td>';
+						}
+						else if(cell.qText.slice(0,4).toLowerCase()==='http'){
+							html += '> <a href="' + cell.qText + '" target="_blank">' + cell.qText + '</a></td>';
+						}
+						else if(cell.qText.slice(0,3).toLowerCase()==='www'){
+							html += '> <a href="http://' + cell.qText + '" target="_blank">' + cell.qText + '</a></td>';
+						}
+						else{
+							html += '>' + cell.qText + '</td>';
+						}
 					}
-				}
-				 
-				  //if just images is selected, check for images and convert
-				else if(layout.imageColumns && !layout.linkColumns){
-					if(~cell.qText.toLowerCase().indexOf('img.') || ~cell.qText.toLowerCase().indexOf('.jpg') || ~cell.qText.toLowerCase().indexOf('.gif') || ~cell.qText.toLowerCase().indexOf('.png')){
-					    html += '> <img src="' + cell.qText + '" height=' + layout.imageHeight + '></td>';
-					}
-					else{
-						html += '>' + cell.qText + '</td>';
-					}
-				}
-				  //if both images and links are selected, if an image, convert and add a link
-				  //if not an image, just convert to link
-				else if(layout.imageColumns && layout.linkColumns){
-					if(~cell.qText.toLowerCase().indexOf('img.') || ~cell.qText.toLowerCase().indexOf('.jpg') || ~cell.qText.toLowerCase().indexOf('.gif') || ~cell.qText.toLowerCase().indexOf('.png')){
-					    html += '> <a href="' + cell.qText + '" target="_blank"><img src="' + cell.qText + '"" height=' + layout.imageHeight + '></a></td>';
-					}
-					else if(cell.qText.slice(0,4).toLowerCase()==='http'){
-						html += '> <a href="' + cell.qText + '" target="_blank">' + cell.qText + '</a></td>';
-					}
-					else if(cell.qText.slice(0,3).toLowerCase()==='www'){
-						html += '> <a href="http://' + cell.qText + '" target="_blank">' + cell.qText + '</a></td>';
-					}
-					else{
-						html += '>' + cell.qText + '</td>';
-					}
-				}
 				  //otherwise, no formatting
-				else{
+					else{
 					html += '>' + cell.qText + '</td>';
+					}
+				}
+				  // value is undefined, fill with null value
+				else{
+					html += '>' + '</td>';
 				}
 
 				});
@@ -160,6 +166,10 @@ define(["jquery", "text!./dpi-simple-table.css"], function($, cssContent) {'use 
 		}
 	};
 });
+
+
+
+
 
 
 
